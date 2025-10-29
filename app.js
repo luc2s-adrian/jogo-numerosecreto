ab634dc97bfa86804746596afcc48d3420d831e4
Lucas Adrian <lucasadrian.cb@gmail.com>
2025-10-29 09:37:28 -0300

    deixando o jogo mais facil

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ elemento.innerHTML = texto;
 }
 function exibirMensagemInicial() {
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Tente advinhar o número entre 1 e 100!');
+exibirTextoNaTela('p', 'Tente advinhar o número entre 1 e 50!');
 }
 
 exibirMensagemInicial();
@@ -29,7 +29,7 @@ else {
 }
 
 function gerarNumeroSecreto() {
-    return Math.floor(Math.random() * 100) + 1;
+    return Math.floor(Math.random() * 50) + 1;
 }
 let numeroSecreto = gerarNumeroSecreto();
 let tentativas = 1;
